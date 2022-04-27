0d45e82a5cfecc8b5fb7668b6c5515e05ad29740
mo7medhasan <mo7med.hasan5@gmail.com>
2022-04-27 23:54:50 +0200

    fix access

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -8,7 +8,7 @@ export default function Register() {
   const password = useRef();
   const passwordAgain = useRef();
   const navigate = useNavigate();
-  const API = process.env.API||"https://git.heroku.com/api-socialmedia.git/api";
+  const API = process.env.API||"https://api-socialmedia.herokuapp.com/api";
   const handleClick = async (e) => {
     e.preventDefault();
    if(passwordAgain.current.value != password.current.value)
